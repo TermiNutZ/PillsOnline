6f3d415fb9c40ee75f6747b9e86986b1485532e4
p.oreshin <nutzpro@gmail.com>
2017-10-28 13:11:22 +0300

    ignore personal settings

--- pills_online/settings.py
+++ pills_online/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from .personal_settings import get_db_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,14 +75,7 @@ WSGI_APPLICATION = 'pills_online.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pills_online',
-	'USER': 'postgres',
-	'PASSWORD': 'PASSWORD',
-	'HOST': 'localhost',
-	'PORT':	'5432'
-    }
+    'default': get_db_settings()
 }
 
 
