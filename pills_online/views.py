e7f5c60cf367927c8bb20934435afdf9732fef6a
p.oreshin <nutzpro@gmail.com>
2017-11-17 18:07:01 +0300

    get_my_drugs

--- pills_online/views.py
+++ pills_online/views.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.models import User, Group
-from main.models import Medication
+from main.models import Medication, Profile
 from rest_framework import viewsets
 
 from pills_online.permissions import RegistrationPermission, GetAuthPermission
 from pills_online.serializers import UserSerializer, GroupSerializer, MedicationSerializer, WarningsAnaloguesSerializer
 from rest_framework.response import Response
 
+
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (RegistrationPermission,)
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
@@ -18,21 +20,26 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+
 class MedicationViewSet(viewsets.ModelViewSet):
-    #queryset = Medication.objects.all().filter(title__startswith='ГЕКСОРАЛ')
+    # queryset = Medication.objects.all().filter(title__startswith='ГЕКСОРАЛ')
     queryset = Medication.objects.all()
     serializer_class = MedicationSerializer
-    """
+
     def get_queryset(self):
         if self.request.query_params['q_type'] == 'all_drugs':
             return Medication.objects.all()
-            #return Medication.objects.all().filter(id=self.request.user.id+3800)
-    """
+        if self.request.query_params['q_type'] == 'my_drugs':
+            user = User.objects.filter(id=self.request.user.id).first()
+            profile = user.profile
+            return profile.medications.all()
+
 
 class WarningsAnaloguesViewSet(viewsets.ModelViewSet):
     permission_classes = (GetAuthPermission,)
     serializer_class = WarningsAnaloguesSerializer
     queryset = Medication.objects.all()
+
     def get_queryset(self):
         query_set = Medication.objects.all().filter(id=self.request.query_params['id'])
         return query_set
